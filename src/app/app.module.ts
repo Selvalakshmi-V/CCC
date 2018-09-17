32ec84f50b9296d3b4c77bbe08d94277a63abbd9
Selvalakshmi <vselvalakshmi24@gmail.com>
2018-09-17 16:08:31 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+/* Feature Components */
+import { PersonalComponent } from './personal/personal.component';
+import { WorkComponent } from './work/work.component';
+import { AddressComponent } from './address/address.component';
+import { ResultComponent } from './result/result.component';
+
+/* Routing Module */
+import { AppRoutingModule } from './app-routing.module';
+
+/* Shared Service */
+import { FormDataService } from './data/formData.service';
+import { WorkflowService } from './workflow/workflow.service';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PersonalComponent,
+    WorkComponent,
+    AddressComponent,
+    ResultComponent,
+    SignupComponent 
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatCardModule,
+    NgbModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule, MatCheckboxModule
+  ],
+  providers: [
+    { provide: FormDataService, useClass: FormDataService },
+    { provide: WorkflowService, useClass: WorkflowService }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
